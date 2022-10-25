1d4ebd7e394b66b28bb51c14aa0d806cd014e6a6
Kadu <kadu1998@outlook.com>
2022-10-25 11:21:14 -0300

    development: code additions and improvements

--- src/App.js
+++ src/App.js
@@ -30,13 +30,28 @@ export default function App() {
             </div>
             <div className="form-control">
               <label htmlFor="age">Idade</label>
-              <Field id="age" name="age" />
+              <Field id="age" name="age" type="number" />
               {errors.age && <p>{errors.age}</p>}
             </div>
             <div className="form-control">
               <label htmlFor="mail">Email</label>
               <Field id="mail" name="mail" type="email" />
               {errors.mail && <p>{errors.mail}</p>}
+            </div>
+            <div className="form-control">
+              <label htmlFor="address">Endereço</label>
+              <Field id="address" name="address" type="text" />
+              {errors.address && <p>{errors.address}</p>}
+            </div>
+            <div className="form-control">
+              <label htmlFor="city">Cidade</label>
+              <Field id="city" name="city" type="text" />
+              {errors.city && <p>{errors.city}</p>}
+            </div>
+            <div className="form-control">
+              <label htmlFor="zipCode">CEP</label>
+              <Field id="zipCode" name="zipCode" type="text" />
+              {errors.zipCode && <p>{errors.zipCode}</p>}
             </div>
             <button id="submit" type="submit">
               Concluir
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './Style.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./Style.css";
+import App from "./App";
+import Header from "./Header";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
+  <div className="mainContainer">
+    <Header />
     <App />
-
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
