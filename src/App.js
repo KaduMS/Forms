c8276d22ab339b11629836f56868fa6ca0675a82
Kadu <kadu1998@outlook.com>
2022-10-25 03:55:03 -0300

    development: first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as yup from "yup";
+import "./Style.css";
 
-function App() {
+
+const schema = yup.object().shape({
+  firstName: yup.string().required(),
+  age: yup.number().min(10).required(),
+});
+
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        validationSchema={schema}
+        initialValues={{
+          firstName: "",
+          age: "",
+          mail: "",
+        }}
+      >
+        {({ errors }) => (
+          <Form>
+            <div className="form-control">
+              <label htmlFor="firtsName">Primeiro Nome</label>
+              <Field id="firstName" name="firstName" type="text" />
+              {errors.firstName && <p>{errors.firstName}</p>}
+            </div>
+            <div className="form-control">
+              <label htmlFor="age">Idade</label>
+              <Field id="age" name="age" />
+              {errors.age && <p>{errors.age}</p>}
+            </div>
+            <div className="form-control">
+              <label htmlFor="mail">Email</label>
+              <Field id="mail" name="mail" type="email" />
+              {errors.mail && <p>{errors.mail}</p>}
+            </div>
+            <button id="submit" type="submit">
+              Concluir
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
-
-export default App;
